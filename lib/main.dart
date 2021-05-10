cf1338b63ec36f306cdcc6c30b312c7404bb0863
umair <umair.shah1817@gmail.com>
2021-05-11 00:43:28 +0500

    my first app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Saylani"),
+        ),
+        body: Center(
+          child: Text(
+            'Umair Zafar',
+            style: TextStyle(
+                fontSize: 30,
+                fontFamily: 'Arial',
+                color: Colors.red,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "my_first_app";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
